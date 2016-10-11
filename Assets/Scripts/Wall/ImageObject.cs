d0b55a38cc33f7fb37b48cba46a303922abcb9e5
Ludovic Feltz <ludovic.feltz@softfluent.com>
2016-10-11 13:29:29 +0200

    Videos

--- Assets/Scripts/Wall/ImageObject.cs
+++ Assets/Scripts/Wall/ImageObject.cs
@@ -4,11 +4,12 @@ using System.Collections;
 
 public class ImageObject : MonoBehaviour {
 
+    [HideInInspector]
 	public Texture Texture;
     
     public void SetImage(Texture texture)
     {
-		this.Texture = Texture;
+		this.Texture = texture;
         var image = GetComponent<RawImage>();
         image.texture = texture;
     }
--- Assets/Scripts/Wall/VideoObject.cs
+++ Assets/Scripts/Wall/VideoObject.cs
@@ -4,10 +4,11 @@ using UnityEngine.UI;
 
 public class VideoObject : MonoBehaviour {
 
+    [HideInInspector]
     public Texture Texture;
     public void SetVideo(Texture texture)
     {
-        this.Texture = Texture;
+        this.Texture = texture;
         var image = GetComponent<RawImage>();
         image.texture = texture;
     }
